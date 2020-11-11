16b776800929836f35b794b2958641c654a6723c
unknown <karan.jagtap195@gmail.com>
2020-11-11 15:24:01 +0530

    Updates

---
+++ precache-manifest.212ba5e09b922ba4312fb405ffbb4bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c1f254d7fc5275e4f735f808c12b0d",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "275961d761c871131284",
+    "url": "/covid19-tracker/static/css/main.d5b5f138.chunk.css"
+  },
+  {
+    "revision": "3560ccb51f7b7ce34166",
+    "url": "/covid19-tracker/static/js/2.fe07179f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.fe07179f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275961d761c871131284",
+    "url": "/covid19-tracker/static/js/main.1f467479.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
